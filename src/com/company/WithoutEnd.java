c1f169c4c2860e3f8e07bf92b66797f575177dd3
RiditaNizam <riditan@gmail.com>
2017-05-11 00:09:23 -0400

    Initial commit

---
+++ src/com/company/WithoutEnd.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by ridita on 5/11/17.
+ */
+public class WithoutEnd {
+    public String withoutEnd(String str) {
+        return str.substring(1, str.length() - 1);
+    }
+}
